f769cee64dbe7445c8a9ada6fcbf7ddbb3e1db69
wzhqwq <wzhii345@outlook.com>
2023-12-27 11:38:32 +0800

    缩减构建目标，仅保留牙龈线生成，添加vcpkg相关配置

--- PyBind.cpp
+++ PyBind.cpp
@@ -14,57 +14,57 @@ PYBIND11_MODULE(OrthoMeshTools, m)
 {
     m.doc() = "A set of tools for ortho scan meshes.";
 
-    m.def("ColorMeshByLabel", &ColorMeshByLabel, "Give each mesh vertex a color according to its label.",
-        py::arg("input_mesh"),
-        py::arg("input_labels"),
-        py::arg("output_mesh"));
+    // m.def("ColorMeshByLabel", &ColorMeshByLabel, "Give each mesh vertex a color according to its label.",
+    //     py::arg("input_mesh"),
+    //     py::arg("input_labels"),
+    //     py::arg("output_mesh"));
 
-    m.def("SegClean", &SegClean,
-        "Try to make sure each label is applied to single connected component. Areas that are smaller than size_threshold are processsed.",
-        py::arg("input_mesh"),
-        py::arg("input_labels"),
-        py::arg("output_labels"),
-        py::arg("size_threshold"));
+    // m.def("SegClean", &SegClean,
+    //     "Try to make sure each label is applied to single connected component. Areas that are smaller than size_threshold are processsed.",
+    //     py::arg("input_mesh"),
+    //     py::arg("input_labels"),
+    //     py::arg("output_labels"),
+    //     py::arg("size_threshold"));
     
-    m.def("FixMesh", &FixMeshFile, "Fix non-manifold vertices & edges",
-        py::arg("path"),
-        py::arg("output_path"),
-        py::arg("keep_largest_connected_component"),
-        py::arg("large_cc_threshold"),
-        py::arg("fix_self_intersection"),
-        py::arg("filter_small_holes"),
-        py::arg("max_hole_edges"),
-        py::arg("max_hole_diam"),
-        py::arg("refine"),
-        py::arg("max_retry"));
+    // m.def("FixMesh", &FixMeshFile, "Fix non-manifold vertices & edges",
+    //     py::arg("path"),
+    //     py::arg("output_path"),
+    //     py::arg("keep_largest_connected_component"),
+    //     py::arg("large_cc_threshold"),
+    //     py::arg("fix_self_intersection"),
+    //     py::arg("filter_small_holes"),
+    //     py::arg("max_hole_edges"),
+    //     py::arg("max_hole_diam"),
+    //     py::arg("refine"),
+    //     py::arg("max_retry"));
 
-    m.def("FixMeshWithLabel", &FixMeshFileWithLabel, "Fix non-manifold vertices & edges and output manipulated vertex label file.",
-        py::arg("path"),
-        py::arg("output_path"),
-        py::arg("input_label"),
-        py::arg("output_label"),
-        py::arg("keep_largest_connected_component"),
-        py::arg("large_cc_threshold"),
-        py::arg("fix_self_intersection"),
-        py::arg("filter_small_holes"),
-        py::arg("max_hole_edges"),
-        py::arg("max_hole_diam"),
-        py::arg("refine"),
-        py::arg("max_retry"));
+    // m.def("FixMeshWithLabel", &FixMeshFileWithLabel, "Fix non-manifold vertices & edges and output manipulated vertex label file.",
+    //     py::arg("path"),
+    //     py::arg("output_path"),
+    //     py::arg("input_label"),
+    //     py::arg("output_label"),
+    //     py::arg("keep_largest_connected_component"),
+    //     py::arg("large_cc_threshold"),
+    //     py::arg("fix_self_intersection"),
+    //     py::arg("filter_small_holes"),
+    //     py::arg("max_hole_edges"),
+    //     py::arg("max_hole_diam"),
+    //     py::arg("refine"),
+    //     py::arg("max_retry"));
 
-    m.def("ReSegment", &ReSegmentLabels, "ReSegment the mesh using the provided splitlines.",
-        py::arg("input_mesh"),
-        py::arg("splitlines"),
-        py::arg("splitline_labels"),
-        py::arg("output_json"),
-        py::arg("intersection_width"),
-        py::arg("cutface_orit_smooth"),
-        py::arg("upper"));
+    // m.def("ReSegment", &ReSegmentLabels, "ReSegment the mesh using the provided splitlines.",
+    //     py::arg("input_mesh"),
+    //     py::arg("splitlines"),
+    //     py::arg("splitline_labels"),
+    //     py::arg("output_json"),
+    //     py::arg("intersection_width"),
+    //     py::arg("cutface_orit_smooth"),
+    //     py::arg("upper"));
 
-    m.def("HoleMerge", &HoleMerge, "Merge the holes on the mesh",
-        py::arg("input_mesh"),
-        py::arg("output_mesh"),
-        py::arg("threshold"));
+    // m.def("HoleMerge", &HoleMerge, "Merge the holes on the mesh",
+    //     py::arg("input_mesh"),
+    //     py::arg("output_mesh"),
+    //     py::arg("threshold"));
 
     m.def("GumTrimLine", &GumTrimLine, "Get gum trim line",
         py::arg("input_mesh"),
