1b20dbd6acb04c2dd6751d978bc3f89b0d0f2099
YangRZ <798245456@qq.com>
2024-02-22 11:09:25 +0800

    add headers

--- OrthoScanBase/OrthoScanBase.h
+++ OrthoScanBase/OrthoScanBase.h
@@ -1,5 +1,6 @@
 #ifndef ORTHO_SCAN_BASE_H
 #define ORTHO_SCAN_BASE_H
+#include <string>
 
 int GenerateGumApi(std::string input_file, std::string input_label, std::string crown_frame, std::string output_gum);
 
